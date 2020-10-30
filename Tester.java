f55a06506701b49d84d7b6fad3ca93c7b7a3bbff
yelsharawy <yelsharawy20@stuy.edu>
2020-10-30 00:20:42 -0400

    Added & tested Point instance variables, constructor, and get methods

--- Tester.java
+++ Tester.java
@@ -1,7 +1,13 @@
 public class Tester {
 
     public static void main(String[] args) {
-
+        System.out.println("Point tests:");
+        Point p = new Point(3, 4);
+        System.out.print("p.getX() -> ");
+        System.out.println(p.getX());  // 3.0
+        System.out.print("p.getY() -> ");
+        System.out.println(p.getY());  // 4.0
+        System.out.println();
     }
-    
+
 }
